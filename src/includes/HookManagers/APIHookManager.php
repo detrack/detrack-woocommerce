d5de9c5364d5db593b502c6f303bafea841d4474
chesnutcase <chester8991@gmail.com>
2018-04-18 16:25:26 +0800

    add error codes for completing delivery api, and also allow trashed orders to also be marked as complete

--- src/includes/HookManagers/APIHookManager.php
+++ src/includes/HookManagers/APIHookManager.php
@@ -5,6 +5,7 @@ namespace Detrack\DetrackWoocommerce\HookManagers;
 use Detrack\DetrackCore\Model\Delivery;
 use WP_REST_Request;
 use WP_REST_Response;
+use WP_Error;
 
 class APIHookManager extends AbstractHookManager
 {
@@ -47,23 +48,33 @@ class APIHookManager extends AbstractHookManager
             $postData = json_decode($request->get_body_params()['json']);
             if (trim($postData->status) == 'Delivered') {
                 $order = wc_get_order($postData->do);
-                if ($order == false) {
-                    return new WP_REST_Response('Order not found, aborting');
+                if ($order == null) {
+                    return new WP_Error('order_not_found', 'Order not found, aborting', array('status' => 404));
                     $this->log('order not found while processing delivery notification, :'.$postData->do, 'error');
+                } elseif ($order->get_status() == 'trash') {
+                    wp_untrash_post($order->get_id());
+                    $order->set_status('completed');
+                    $order->save();
+                    wp_trash_post($order->get_id());
+                    $this->log('delivery completed for order in trash : '.$postData->do.' but moved back to trash', 'debug');
+
+                    return new WP_REST_Response('delivery completed for order in trash : '.$postData->do.' but moved back to trash');
                 }
                 $order->set_status('completed');
                 $order->save();
-                $this->log('delivery completed for order :'.$postData->do, 'debug');
+                $this->log('delivery completed for order : '.$postData->do, 'debug');
+
+                return new WP_REST_Response('delivery completed for order :'.$postData->do, 'debug');
             } else {
                 $this->log('Status of posted data of DO'.$postData->do.' is not Delivered, aborting.');
                 $this->log('body_params:'.var_export($postData, true), 'error');
 
-                return new WP_REST_Response('Status of Posted Data is not Delivered, aborting.');
+                return new WP_Error('status_not_delivered', 'Status of Posted Data is not Delivered, aborting.', array('status' => 400));
             }
         } catch (\Exception $ex) {
             $this->log('processing delivery notification failed: '.$ex->getMessage(), 'error');
 
-            return new WP_REST_Response('Error:'.$ex->getMessage());
+            return new WP_Error('internal_server_error', 'Error:'.$ex->getMessage(), array('status' => 500));
         }
     }
 }
