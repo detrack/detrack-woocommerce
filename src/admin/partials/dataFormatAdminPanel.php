080d185b8f7f8df48791300ea161233815443ffa
chesnutcase <chester8991@gmail.com>
2018-07-02 21:00:44 +0800

    show easy/expert mode again

--- src/admin/partials/dataFormatAdminPanel.php
+++ src/admin/partials/dataFormatAdminPanel.php
@@ -14,9 +14,9 @@
           <!-- Temporarily hide Expert mode, only release in future updates
                If you read this, feel free to re-enable this
           !-->
-          <ul style="background-color:#e5e5e5;;border:0px;display:none">
-            <li style="border:1px;background-color:#e5e5e5;"><a href="#detrack-attribute-mapping-easy">Easy modo</a></li>
-            <li style="border:1px;background-color:#e5e5e5;"><a href="#detrack-attribute-mapping-expert">Expert modo</a></li>
+          <ul style="background-color:#e5e5e5;;border:0px;display:block">
+            <li style="border:1px;background-color:#e5e5e5;"><a href="#detrack-attribute-mapping-easy">Easy mode</a></li>
+            <li style="border:1px;background-color:#e5e5e5;"><a href="#detrack-attribute-mapping-expert">Expert mode</a></li>
           </ul>
           <div id="detrack-attribute-mapping-easy" style="margin:0px;padding:0px;border:0px;">
             <table class="wc_gateways widefat">
