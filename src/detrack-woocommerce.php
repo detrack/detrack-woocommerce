575987609e8bbf5d18475f441258f56b43d20af8
chesnutcase <chester8991@gmail.com>
2018-07-23 07:55:17 +0800

    update header version to 1.1.1

--- src/detrack-woocommerce.php
+++ src/detrack-woocommerce.php
@@ -10,7 +10,7 @@
  * Plugin Name: Detrack Woocommerce
  * Plugin URI:  https://www.detrack.com/tutorials/woocommerce-integration/
  * Description: Integration with the Detrack API
- * Version:     1.1
+ * Version:     1.1.1
  * Author:      Detrack
  * Author URI:  https://detrack.com
  * Text Domain: detrack-woocommerce
